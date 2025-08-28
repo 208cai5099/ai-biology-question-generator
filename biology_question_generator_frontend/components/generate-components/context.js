7f509fc830db5dd43f2883ae3364955aa0566ffe
leven <208cai5099@gmail.com>
2025-08-28 12:20:53 -0400

    Removed previous files to re-start from scratch using TS

--- biology_question_generator_frontend/components/generate-components/context.js
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { createContext } from "react"
-
-// this formContext is used in GenerateForm() to pass down variables and functions
-// for keeping track of user inputs 
-export const formContext = createContext({})
